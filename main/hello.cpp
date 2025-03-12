27eaa34ecc0e13cfe84268fda51c197cdb46ad1b
Siri N Shetty <sirishetty.narendra@gmail.com>
2025-03-12 18:39:38 +0000

    Testing webhook trigger

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "I have added GitHub Webhooks\n";
+  cout << "I have added a GitHub Webhook\n";
   return 0;
 }
